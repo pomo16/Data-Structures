97aad05fc47814e666ff9534cb2296e42a4cfe94
pomo <m13143378602@163.com>
2019-03-24 22:10:57 +0800

    Commit LinkedListStack

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addFirst(4);
+        System.out.println(ll.toString());
+    }
+}
