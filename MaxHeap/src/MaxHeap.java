c5be9be02b94cc421821204216d5d9e859e5bc4d
pomo <m13143378602@163.com>
2019-03-28 14:52:11 +0800

    Update MaxHeap.

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -46,7 +46,44 @@ public class MaxHeap<E extends Comparable<E>> {
 
     private void siftUp(int k){
         while(k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0){
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
 
+    // 看堆中的最大元素
+    public E findMax(){
+        if(data.getSize() == 0){
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        }
+        return data.get(0);
+    }
+
+    // 取出堆中最大元素
+    public E extractMax(){
+        E ret = findMax();
+
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+
+    private void siftDown(int k){
+        while(leftChild(k) < data.getSize()){
+            int j = leftChild(k);
+            if(j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(k);
+            }
+            // data[j] 是 leftChild 和 rightChild 中的最大值
+
+            if(data.get(k).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+
+            data.swap(k, j);
+            k = j;
         }
     }
 }
